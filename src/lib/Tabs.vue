ba91cd22856c78ba68de9349cd321cf99fecaa3a
CHEN <812607138@qq.com>
2023-07-25 02:47:45 +0800

    indicator css

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -2,6 +2,7 @@
 <div class="wind-tabs">
   <div class="wind-tabs-nav">
     <div class="wind-tabs-nav-item" v-for="(t,index) in titles" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
+    <div class="gulu-tabs-nav-indicator"></div>
   </div>
   <div class="wind-tabs-content">
     <component class="wind-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults" :is="c" />
@@ -62,6 +63,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -72,6 +74,14 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
     }
   }
   &-content {
